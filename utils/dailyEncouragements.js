c169a5634e940d549288f3a1f744e7f31f57d94d
Tyclypse <90060882+Tyclypse@users.noreply.github.com>
2026-06-27 05:09:16 -0400

    Add daily encouragement message pool

---
+++ utils/dailyEncouragements.js
@@ -0,0 +1,33 @@
+const DAILY_ENCOURAGEMENTS = [
+    "🌟 did you guys drink... water... yet...? please do. i worry.",
+    "☀️ good afternoon!! i hope today's treating everyone well today. 💛",
+    "✨ another day, another update! (hopefully 👀)",
+    "🌈 who's happy today? we need more happiness around here.",
+    "💙 how's everyone doing this fine day?",
+    "🌻 be positive, and think of the bright side of life! (help me....)",
+    "🍞 reminder to eat something today!! coding on an empty stomach is NOT recommended.",
+    "💤 don't forget to get some sleep tonight! your future self will appreciate it.",
+    "🚰 hydration checkpoint!! go drink some water. yes, YOU. i'm watching.",
+    "🎨 every little bit of progress counts. even fixing one tiny bug is still progress!",
+    "⭐ if today ends up being slow, that's okay. tomorrow's a brand new day.",
+    "🫂 just checking in... i hope everyone's doing alright today. take care of yourselves. 💛",
+    "🐢 don't compare your progress to someone else's. everyone's moving at their own pace.",
+    "🎮 who's working on something cool today? i'd love to hear about it!!",
+    "📦 today's mission: make at least ONE thing that didn't exist yesterday.",
+    "🍀 remember: bugs are temporary. awesome games are forever.",
+    "🤖 i performed today's studio inspection. conclusion: you all are pretty cool.",
+    "🧀 i couldn't think of anything motivational today... so here's some emotional support cheese. 🧀",
+    "👀 imagine if everyone here made just one small improvement today. we'd be unstoppable.",
+    "💛 thank you all for helping make Starfloat Studios what it is. i'm really happy to be here with you guys."
+];
+
+function getRandomEncouragement() {
+    return DAILY_ENCOURAGEMENTS[
+        Math.floor(Math.random() * DAILY_ENCOURAGEMENTS.length)
+    ];
+}
+
+module.exports = {
+    DAILY_ENCOURAGEMENTS,
+    getRandomEncouragement,
+};
